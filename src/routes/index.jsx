77d1c24f6cb92a2f9ed16e3f779aeaceee28937f
Luis Hernandez <luishernandezopsu2019@gmail.com>
2026-04-20 21:21:10 -0400

    version final del proyecto de react router

---
+++ src/routes/index.jsx
@@ -0,0 +1,13 @@
+export default function Index(){
+    return (
+        <p id="zero-state">
+            this is a demo for React Route
+            <br />
+            Check out{" "}
+            <a href="https://reactrouter.com">
+                the docs at reactrouter.com
+            </a>
+            .
+        </p>
+    );
+}
